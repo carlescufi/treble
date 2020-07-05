6d90a0d2a34741f7b26b44572c2124b82edb27a6
Carles Cufi <carles.cufi@nordicsemi.no>
2020-07-05 20:38:39 +0200

    sb: Add some logging tests
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- sb.py
+++ sb.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+import logging
 import threading
 import asyncio
 import time
+import sys
+
 
 async def say_after(delay, what):
     print(f'say_after: {delay} thread:{threading.current_thread()}'
@@ -19,8 +22,29 @@ async def aio_sb():
     await task2
     print(f"finished at {time.strftime('%X')} on {threading.current_thread()}")
 
+def log_layer():
+    logger = logging.getLogger('sb.layer')
+    logger.info('test log_layer')
+
+def log_sb():
+    logger = logging.getLogger('sb')
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s -'
+            ' %(message)s', datefmt='%H:%M:%S')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.info('test log_sb')
+    log_layer()
+
+    #logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s -'
+    #        ' %(levelname)s - %(message)s', datefmt='%H:%M:%S')
+    #logging.debug(f"debug test: {sys.argv[0]}")
+
 def main():
-    asyncio.run(aio_sb())
+    log_sb()
+    #asyncio.run(aio_sb())
 
 if __name__ == '__main__':
     main()
